e521ec3a2a9869a5ea53c64924a93aa0fb31dbcf
Debora Polesel <deborahpolesel@hotmail.com>
2020-06-30 00:36:53 -0300

    Resolução exercicio 6

--- exercicio6.sh
+++ exercicio6.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# EXERCICIO 6:
+FILE=$1
+if [ -f "$FILE" ]
+  then
+    echo "$FILE é um arquivo comum"
+    else if [ -d "$FILE" ]
+    then
+        echo "$FILE é um diretório"
+    else
+        echo "$FILE é alguma outra coisa"
+    fi
+fi
+ls -l $FILE
